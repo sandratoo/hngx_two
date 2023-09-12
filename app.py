e1b0ffb0053a881972652e32f4b9e96256e80c89
sandratoo <toosandraa@gmail.com>
2023-09-12 18:35:09 +0300

    CRUD complete

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "SECRETKEY"
 
 db = SQLAlchemy(app)
 
@@ -30,22 +31,61 @@ def fetch_all():
         data = {"id":person.id,"firstname":person.firstname,"lastname":person.lastname,"job":person.job}
         output.append(data)
 
-    return {"persons": output}
+    return jsonify({"persons": output})
     
 #Read one specific person
-@app.route("/api/{person_id}")
-def fetch_person():
-    return "person"
+@app.route("/api/<id>", methods=["GET"])
+def fetch_person(id):
+    person = Person.query.filter_by(id=id).first()
+    if person is None:
+        abort(404)
+
+    person_data = {"firstname":person.firstname,"lastname":person.lastname,"job":person.job}
+
+    return jsonify({person.id: person_data})
 
 #Create a new person
-@app.route("/api/users", methods=["POST"])
+@app.route("/api", methods=["POST"])
 def add_person():
     person = Person(firstname=request.json["firstname"],
      lastname=request.json["lastname"],
-      job=request.json["job"])
+     job=request.json["job"])
+
     db.session.add(person)
     db.session.commit()
-    return{person.id: person.firstname}
+
+    person_data = {"firstname":person.firstname,"lastname":person.lastname,"job":person.job}
+
+    return jsonify({person.id: person_data})
+
+#Update an existing person
+@app.route("/api/<id>", methods=["PUT"])
+def update_person(id):
+    person = Person.query.filter_by(id=id).first()
+
+    if person is None:
+        abort(404)
+    person.firstname=request.json["firstname"]
+    person.lastname=request.json["lastname"]
+    person.job=request.json["job"]
+        
+    db.session.commit()
+
+    person_data = {"firstname":person.firstname,"lastname":person.lastname,"job":person.job}
+
+    return jsonify({person.id: person_data})
+        
+#Delete a person
+@app.route("/api/<id>", methods=["DELETE"])
+def delete_person(id):
+    person = Person.query.filter_by(id=id).first()
+
+    if person is None:
+        abort(404)
+    db.session.delete(person)
+    db.session.commit()
+
+    return jsonify({person.id : "deleted"})
 
 
 if __name__ == ("__main__"):
